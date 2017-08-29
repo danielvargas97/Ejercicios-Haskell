9cf6a805b3da8f85d5739b8cb24711843611df93
Daniel Vargas <danielvargas999@gmail.com>
2017-08-29 18:48:53 -0500

    Se cambia el parametro y la salida a Integer para soportar numeros mas grandes

--- ConversorBinarioDecimal.hs
+++ ConversorBinarioDecimal.hs
@@ -1,9 +1,9 @@
-decbin :: Int->Int
+decbin :: Integer->Integer
 decbin 0 = 0
 decbin 1 = 1
 decbin n = (n`mod`2)+10*decbin(n`div`2)
 
-bindec :: Int->Int
+bindec :: Integer->Integer
 bindec 0 = 0
 bindec 1 = 1
 bindec n = (n`mod`10)+2*bindec(n`div`10)
