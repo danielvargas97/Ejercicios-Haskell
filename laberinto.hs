50c85f4ba3ddab8f612870d9ae7b807813d1f823
Yohansen A <oscartopin1@gmail.com>
2017-09-19 13:46:30 -0500

    Se agrega direccion Arriba y Abajo. Falta implementar camino con lista

--- laberinto.hs
+++ laberinto.hs
@@ -1,5 +1,6 @@
 matriz :: [[Char]]
-matriz = [ ['1','1','1','1','1','1','1','1'],
+matriz = [ 
+	   ['1','1','1','1','1','1','1','1'],
 	   ['1','1','1','1','1','1','1','1'],
 	   ['1','F','0','0','1','1','1','1'],
 	   ['1','1','X','0','1','1','1','1'],
@@ -7,47 +8,36 @@ matriz = [ ['1','1','1','1','1','1','1','1'],
 	   ['1','1','1','1','1','1','1','1']]
 
 
-indice :: [[Char]]->Int->Int->Char
-indice x fila columna = (x!!fila)!!columna
-
-
+-- Muestra el elemento en la posicion (fila,columna)
 indicex :: [[Char]]->(Int,Int)->Char
 indicex y (fila,columna) =  (y!!fila)!!columna
 
-encontrarEquis::[[Char]]->Int->Int->String
-encontrarEquis x fila columna = if indice x fila columna == 'X' then "Si hay x" else  encontrarEquis x (fila+1) (columna)
- 
 
-
-
-moverHorizontal :: [Char]->Char->Int->Int
-moverHorizontal [] _ _ = (-1)
-moverHorizontal (x:xs) letra columna = if x == letra then columna else moverHorizontal xs letra (columna+1)
-
-
-moverVertical :: [[Char]]->Char->Int->(Int,Int)
-moverVertical (x:xs) letra fila = if moverHorizontal x letra 0 /= (-1) then (fila, moverHorizontal x letra 0) else moverVertical xs letra (fila+1)
-moverVertical [] _ _ = (-1,-1)
-
+-- Mover la coordenada a la izquierda
 moverIzquierda :: (Int,Int)->(Int,Int)
 moverIzquierda (a,b) = (a,b-1)
 
+
+-- Mover la coordenada a la derecha
 moverDerecha :: (Int,Int)->(Int,Int)
 moverDerecha (a,b) = (a,b+1)
 
+-- Mover la coordenada a la derecha
+moverArriba :: (Int,Int)->(Int,Int)
+moverArriba (a,b) = (a-1,b)
+
+-- Mover la coordenada a la izquierda
+moverAbajo :: (Int,Int)->(Int,Int)
+moverAbajo (a,b) = (a+1,b)
+
+
+--Halla la posicion inicial del laberinto
 posicionInicial :: [[Char]]->(Int,Int)
 posicionInicial x = moverVertical x 'X' 0
  
-
+--Halla la posicion final del laberinto
 posicionFinal :: [[Char]]->(Int,Int)
 posicionFinal x = moverVertical x 'F' 0
 
-
-laberinto :: [[Char]]->(Int,Int)->(Int,Int)->[(Int,Int)]->[(Int,Int)]
-
-laberinto x (a,b) (c,d) w = if (a,b) == posicionFinal x 
-								then [(a,b)] 
-							else 
-								[(c,d)]
 
 
